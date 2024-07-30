5f06179476913e478cdbc1e2a36858a46419706d
AntoReviriego <65468918+AntoReviriego@users.noreply.github.com>
2024-07-29 21:49:33 -0300

    Se agrega liberia de clases

---
+++ EjercicioBlazor/SharedClasses/CategoriaDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedClasses
+{
+    public class CategoriaDTO
+    {
+        public long Id { get; set; }
+
+        public string Descripcion { get; set; } = null!;
+
+        public long? IdCategoriaPadre { get; set; }
+
+        public DateTime InsertDate { get; set; }
+
+        public DateTime? UpdateDate { get; set; }
+
+        public bool Enable { get; set; }
+    }
+}
---
+++ EjercicioBlazor/SharedClasses/MarcaDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedClasses
+{
+    public class MarcaDTO
+    {
+        public long Id { get; set; }
+
+        public string Descripcion { get; set; } = null!;
+
+        public DateTime InsertDate { get; set; }
+
+        public DateTime? UpdateDate { get; set; }
+
+        public bool Enable { get; set; }
+    }
+}
